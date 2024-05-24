73d97f13b9aaba72c4044fbf2af58b01ce4feca7
Neo Krypt <neo@canxium.org>
2024-05-24 10:47:20 +0700

    Update praseody testnet config

--- config/params/testnet_praseody_config.go
+++ config/params/testnet_praseody_config.go
@@ -7,7 +7,7 @@ func UsePraseOdyNetworkConfig() {
 	cfg := BeaconNetworkConfig().Copy()
 	cfg.ContractDeploymentBlock = 0
 	cfg.BootstrapNodes = []string{
-		// lighthouse
+    	// lighthouse
 		"enr:-Le4QGgkMit_S6dcseu-6HUrQLKgXsOqjGmXvALew2xjINBwFeAGMGufIfOLQZoDXEBhqAnhjMW6eXufW81mVUNnnaMChGV0aDKQ2jJYTwAyMwABAAAAAAAAAIJpZIJ2NIJpcIQFobgAg2lwNpAqAQT_APCN3QAAAAAAAAABiXNlY3AyNTZrMaECNQgaxqD0o_mb6YMG5YDwIm0GhXMJF0iEuvAb-Sjl9KKDdWRwgiOMhHVkcDaCI-Y", // 40
 		"enr:-Le4QJFLssDaSa99PqSFZVmzYPLFjLR0Glf5sLtNhluY1p4gV0NG1AWNiluckVx4ZNq7QCB_q-RbNUSvjZZ-wJl6XzYBhGV0aDKQ2jJYTwAyMwABAAAAAAAAAIJpZIJ2NIJpcIQxDY3Wg2lwNpAqAQT4wBI7yQAAAAAAAAABiXNlY3AyNTZrMaEDAnVP2fJn3ZQU2NBaoPOWYtKl7_JP1-6AMrZkMYM-x4CDdWRwgiOMhHVkcDaCI-Y", // 14
 		"enr:-Le4QApx4yDdnYodma6kKim7v_UARV1B1AZ6fBMoMX2tM_gJQzOaY_lGfJ-e_q_3ZqZGavyIfhWme5SmdQTipYNa1bMBhGV0aDKQ2jJYTwAyMwABAAAAAAAAAIJpZIJ2NIJpcISyEInRg2lwNpAqAkeAABIjpwAAAAAAAAABiXNlY3AyNTZrMaEDCUfXoP8UZUHaVQpm8TQRhm-xWGtyHbAsi5hZxmUinhGDdWRwgiOMhHVkcDaCI-Y", // 09
@@ -18,8 +18,9 @@ func UsePraseOdyNetworkConfig() {
 		"enr:-KG4QDWqJ8wxnw4JHqQzxVSGSet4q8osBH568XkMnC1u8fPnPFARnkPIAq8KKEa8JKi6kN1EFSFkfnv8k6ZeWJPXTSkBgmlkgnY0gmlwhLIQidGDaXA2kCoCR4AAEiOnAAAAAAAAAAGJc2VjcDI1NmsxoQOEVvdJhjDQB6zwUNjYCNTvPXtCSKc2UqW5x2VoG5Gp04N1ZHCCI42EdWRwNoIj5w", // 17
 
 		// static nodes
-		"enr:-Nm4QHzN2cYmgnYqrK7dCNNnP44QP328uwJ0sWjLlDGNAhY4MfeaeEmFJ9kLeXY2-AYlfx8cwHTcjjbL1Cl_5AnqGXsFh2F0dG5ldHOIAAAAAIABAACEZXRoMpCceb3AADJTAP__________gmlkgnY0gmlwhA_rjYiDaXA2kCQCHwCAAAgAAAAAAAAALNyJc2VjcDI1NmsxoQNHAqh0K2EFoHK18tyrGNUSlLWs-5I93z2lfsXbEQd25ohzeW5jbmV0c4gAAAAAAAAAAIN0Y3CCIy6DdWRwgiMu", // lodestar
-		"enr:-MK4QDZ9Vg_ty6msUFWiQC8aT4U_rZBS3Y3HPz5YfokuX6ipA0Mx6sqdn6-5vE1CMeS3GnFAHdvvhsd5SDLKB-vNgdKGAY70dagYh2F0dG5ldHOIAAAAAAAAAACEZXRoMpCceb3AADJjAP__________gmlkgnY0gmlwhMMjLZuJc2VjcDI1NmsxoQP9fEckcrTin5d_WCq0cBy5RurdNFCC-Z42IjDUlKvwRYhzeW5jbmV0cwCDdGNwgjLKg3VkcIIu4g",                               // prysm
+		"enr:-KC4QIhraGmuvYKCiZX5O5dzIXRRCsL1vYUo7F9DEzeMUI1XGXXrz7d8COU4IKtluOGjpNCpOgS8uvsZXR2eoVpFiRsBgmlkgnY0gmlwhA_rjYiDaXA2kCQCHwCAAAgAAAAAAAAALNyJc2VjcDI1NmsxoQMSRV2gTlqXdgWCMHjGpRWl7v9uxn5Fg7BGlN_bc43Nf4N0Y3CCIyqDdWRwgiMq", // ovh 136
+		"enr:-Iu4QGWVVetaMn9EbBzLYYWKWvISuB70mpZ0CIontoN0hDsYD3sbAo5HoCfXO5m4TLjlKku0AIrNboXjQrnj1oKTXrQCgmlkgnY0gmlwhE3t8NOJc2VjcDI1NmsxoQNk7zs3J_uADDHgjZ8yRJ3WlneOd955rQF0wNFGyOydFIN0Y3CCIyyDdWRwgiMs", // 211
+		"enr:-Iu4QCKlcenV8UzhV4VjnvXJzL2Cguuw6BrmXrLqdz6vRk7IGGWLmPZcY6Q2tU5ivLIZN312i8HYjztD7SnB4ppMTv8BgmlkgnY0gmlwhE3t8NKJc2VjcDI1NmsxoQI1g-K4llSsqpZysW2PhvPUCjMUri-NrfTqyUL4AE4mgYN0Y3CCIyyDdWRwgiMs", // 210
 	}
 	OverrideBeaconNetworkConfig(cfg)
 }
@@ -29,10 +30,10 @@ func PraseOdyConfig() *BeaconChainConfig {
 	cfg := MainnetConfig().Copy()
 	cfg.ConfigName = PraseOdyName
 
-	cfg.MinGenesisTime = 1712311200
-	cfg.MinGenesisActiveValidatorCount = 164
+	cfg.MinGenesisTime = 1716360347
+	cfg.MinGenesisActiveValidatorCount = 132
 
-	cfg.GenesisDelay = 600
+	cfg.GenesisDelay = 46800
 
 	cfg.DepositChainID = 30203
 	cfg.DepositNetworkID = 30203
@@ -48,16 +49,16 @@ func PraseOdyConfig() *BeaconChainConfig {
 	cfg.DenebForkEpoch = math.MaxUint64
 	cfg.DenebForkVersion = []byte{0x00, 0x32, 0x63, 0x00}
 
-	cfg.TerminalTotalDifficulty = "1016000000"
+	cfg.TerminalTotalDifficulty = "762047059"
 
 	cfg.SecondsPerSlot = 6
 	cfg.SecondsPerETH1Block = 6
 	cfg.Eth1FollowDistance = 128
 
-	cfg.BaseRewardFactor = 0
-	cfg.InactivityPenaltyQuotientBellatrix = 1 << 24
+	cfg.BaseRewardFactor = 512
+	cfg.InactivityPenaltyQuotientBellatrix = 1 << 22
 	cfg.MaxExcessBalance = 32 * 1e10
-
+	
 	cfg.InitializeForkSchedule()
 	return cfg
 }
